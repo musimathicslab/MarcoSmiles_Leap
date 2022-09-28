f4351d23c05d0c547a3c6f43886c702d4df6e546
DarioTroll <puddino1337@gmail.com>
2022-09-28 14:40:53 +0200

    Guide updated and new files added

---
+++ Installation Files/test.bat
@@ -0,0 +1,5 @@
+call activate gputest
+call conda env list
+call magenta_midi --list_ports
+call magenta_midi --input_ports="MarcoSmiles 0" --output_ports="MarcoJams 2" --bundle_files="attention_rnn.mag"
+timeout /t 150
